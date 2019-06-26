972ba63b1820d9c4666f8bd82c8990ac66801f59
Sumit Kumar Kundu <sumit_besu@rediffmail.com>
2019-06-26 13:09:41 +0000

    initial push

---
+++ webapp/Component.js
@@ -0,0 +1,7 @@
+sap.ui.define(["sap/suite/ui/generic/template/lib/AppComponent"], function (AppComponent) {
+	return AppComponent.extend("sos.editop.Component", {
+		metadata: {
+			"manifest": "json"
+		}
+	});
+});
